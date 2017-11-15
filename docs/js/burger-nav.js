b2ee697f680b04aca474bd5a906c519ebf6c4731
AnteroEscham <hurtinandr@gmail.com>
2017-11-15 18:17:03 +0300

    for site hwork3

--- docs/js/burger-nav.js
+++ docs/js/burger-nav.js
@@ -2,7 +2,6 @@ let burger = document.getElementById('menu-burger');
 let burgerClosed = document.getElementById('burger-close');
 let mobileMenu = document.getElementById('mobile-menu');
 let navLink = document.getElementsByClassName('mobile-menu__link');
-console.log(navLink);
 burger.addEventListener('click', function () {
   mobileMenu.style.display = 'block';
 });
@@ -11,7 +10,7 @@ burgerClosed.addEventListener('click', function () {
   mobileMenu.style.display = 'none';
 });
 
-for(i=0; i<navLink.length; i++) {
+for(let i=0; i<navLink.length; i++) {
   navLink[i].addEventListener('click', function () {
     mobileMenu.style.display = 'none';
   });
