0199818e2a5f7a4e25086d1ea324ec13841eb32f
AnteroEscham <hurtinandr@gmail.com>
2017-11-19 13:19:12 +0300

    for site

--- docs/js/burger-nav.js
+++ docs/js/burger-nav.js
@@ -1,21 +1,23 @@
-let burger = document.getElementById('menu-burger');
-let burgerClosed = document.getElementById('burger-close');
-let mobileMenu = document.getElementById('mobile-menu');
-let navLink = document.getElementsByClassName('mobile-menu__link');
-let body = document.body;
-burger.addEventListener('click', function () {
-  mobileMenu.style.display = 'block';
-  body.classList.add('lock');
-});
+$(function () {
+  const burger = document.getElementById('menu-burger');
+  const burgerClosed = document.getElementById('burger-close');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const navLink = document.getElementsByClassName('mobile-menu__link');
+  const body = document.body;
+  burger.addEventListener('click', function () {
+    mobileMenu.style.display = 'block';
+    body.classList.add('lock');
+  });
 
-burgerClosed.addEventListener('click', function () {
-  mobileMenu.style.display = 'none';
-  body.classList.remove('lock');
-});
-
-for(let i=0; i < navLink.length; i++) {
-  navLink[i].addEventListener('click', function () {
+  burgerClosed.addEventListener('click', function () {
     mobileMenu.style.display = 'none';
     body.classList.remove('lock');
   });
-}
+
+  for(let i=0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', function () {
+      mobileMenu.style.display = 'none';
+      body.classList.remove('lock');
+    });
+  }
+});
