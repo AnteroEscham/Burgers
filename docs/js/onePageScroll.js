8a52326a6bc4b4c05e1a1263be34b47ac2e68e0e
AnteroEscham <hurtinandr@gmail.com>
2017-11-21 20:11:11 +0300

    for site

--- docs/js/onePageScroll.js
+++ docs/js/onePageScroll.js
@@ -1,38 +1,121 @@
-$(function() {
-  let
-  screen = 0,
-  container = $('.main-content'),
-  pages = $('.section'),
-  inscroll = false;
+$(function () {
+  const display = $('.main-content');
+  const sections =  $('.section');
+  let inScroll = false;
 
-  $('.section:first-child').addClass('active');
+  const mobileDetect = new MobileDetect(window.navigator.userAgent);
+  const isMobile = mobileDetect.mobile();
 
-$('body').on('mousewheel', (e) => {
+  const switchMenuActiveClass = sectionEq => {
+    $('.fix-nav__item').eq(sectionEq).addClass('fix-nav__item--active')
+    .siblings().removeClass('fix-nav__item--active');
+  }
 
-  let activePage = pages.filter('.active')
-    if(!inscroll) {
-      inscroll = true;
+  const moveTo = sectionEq => {
+    const position = (sectionEq * -100) + '%';
+    if (inScroll == false) {
+      inScroll = true;
+      display.css({
+        'top': position
+      })
 
-      if(e.deltaY > 0) {
+      sections.eq(sectionEq).addClass('active').siblings().removeClass('active');
 
-        if(activePage.prev().length) {
-          screen--;
-        }
-    } else {
-
-      if(activePage.next().length) {
-          screen++;
-        }
+      setTimeout(() => {
+        inScroll = false;
+        switchMenuActiveClass(sectionEq);
+      }, 1300);
     }
+
+  }
+
+  const defineSection = sections => {
+    const sectionActive = sections.filter('.active');
+    return {
+      sectionActive: sectionActive,
+      nextSection: sectionActive.next(),
+      prevSection: sectionActive.prev()
+    }
+  }
+
+  const scrollToSection = direction => {
+    const section = defineSection(sections);
+
+    if (inScroll) return;
+
+    if (direction == 'up' && section.nextSection.length) {
+      moveTo(section.nextSection.index());
     }
 
-    setTimeout(() => {
-      inscroll = false;
-    }, 1300)
+    if (direction == 'down' && section.prevSection.length) {
+      moveTo(section.prevSection.index());
+    }
 
-    let position = (-screen*100) + '%';
-    container.css('top', position);
-    pages.eq(screen).addClass('active').siblings().removeClass('active');
+
+
+  }
+
+  $('.wrapper').on({
+    wheel: (e) => {
+      const deltaY = e.originalEvent.deltaY;
+      let direction = (deltaY > 0) ? 'up' : 'down';
+
+      scrollToSection(direction);
+
+    touchmove: e => {
+      e.preventDefault();
+    }
+  }
+
+})
+
+  $(document).on('keydown touchstart', e => {
+    const section = defineSection(sections);
+
+    if(inScroll) return
+
+    switch (e.keyCode) {
+      case 40:
+        if(!section.nextSection.length) return
+        moveTo(section.nextSection.index());
+        break;
+
+      case 38:
+      if(!section.prevSection.length) return
+      moveTo(section.prevSection.index());
+        break;
+
+    }
+  })
+
+  if (isMobile) {
+    $(window).swipe( {
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      scrollToSection(direction);
+    }
+  });
+  }
+
+  $('[data-scroll-to]').on('click', e => {
+    e.preventDefault();
+    const thisElem = $(e.currentTarget);
+    const sectionIndex = parseInt(thisElem.attr('data-scroll-to'));
+    moveTo(sectionIndex);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
 
@@ -42,4 +125,90 @@ $('body').on('mousewheel', (e) => {
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   let
+//   screen = 0,
+//   container = $('.main-content'),
+//   pages = $('.section'),
+//   inscroll = false;
+//
+//   $('.section:first-child').addClass('active');
+//
+// $('body').on({
+//   wheel: (e) => {
+//     let deltaY = e.originalEvent.deltaY;
+//
+//     let activePage = pages.filter('.active')
+//       if(!inscroll) {
+//         inscroll = true;
+//
+//         if(deltaY < 0) {
+//
+//           if(activePage.prev().length) {
+//             screen--;
+//           }
+//       } else {
+//
+//         if(activePage.next().length) {
+//             screen++;
+//           }
+//       }
+//       }
+//
+//       setTimeout(() => {
+//         inscroll = false;
+//       }, 1300)
+//
+//       let position = (-screen*100) + '%';
+//       container.css('top', position);
+//       pages.eq(screen).addClass('active').siblings().removeClass('active');
+//
+//   }
+// })
+//
+//
+//
+//
+//
+//
+//
+//
+// })
