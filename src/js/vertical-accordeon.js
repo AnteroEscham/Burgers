f9a268ca41580ceff3bc5c24fe9e78392bb2fd16
AnteroEscham <hurtinandr@gmail.com>
2017-11-28 23:01:29 +0300

    build with gulp

---
+++ src/js/vertical-accordeon.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('.team__about-name').slideUp(0);
+  $('.team__name').on('click', (e) => {
+    let thisElem = e.currentTarget;
+   $(thisElem).siblings('.team__about-name').slideToggle(300);
+   $('.team__about-name').not($(thisElem).next()).slideUp(300).parent().removeClass('team__item--active');
+   $(thisElem).parent().toggleClass('team__item--active');
+  })
+});
